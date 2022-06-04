b0a7bdfd3e3d53826a2da0d8721f358b832f76c6
Victor Gaultney <victor_gaultney@sil.org>
2022-06-04 16:39:51 +0100

    Add tiles and lines

--- example.py
+++ example.py
@@ -7,19 +7,71 @@ filename = "./test1.pdf"
 
 # Define standard page, frame, mat, design
 page = ruleup.Canvas(
-    pageheight = 4000,
-    frame = { 'visible': True, 'top' : 100, 'right': 100, 'bottom': 100, 'left': 100, 'width': 0, 'fill': [.3,.2,0,1] },
-    mat = { 'visible': True, 'top' : 150, 'right': 250, 'bottom': 350, 'left': 450, 'width': 0, 'fill': [0,.3,0,1] },
-    design = { 'top' : 100, 'right': 100, 'bottom': 100, 'left': 100 }
+    pageheight = 4200,
+    pagewidth = 2970,
+    frame = { 'visible': True, 'top' : 100, 'right': 100, 'bottom': 100, 'left': 100, 'swidth': 0, 'fill': [.3,.2,0,1] },
+    mat = { 'visible': True, 'top' : 100, 'right': 100, 'bottom': 100, 'left': 100, 'swidth': 0, 'fill': [0,.3,0,1] },
+    design = { 'visible': True, 'top' : 100, 'right': 100, 'bottom': 100, 'left': 100, 'swidth': 1, 'color': [0,0,1,.2] }
+)
+
+# Define individual tiles and their contents
+tile_main = ruleup.Tile(
+    visible = True,
+    align = page.dtopl,
+    offset = (200, -150),   # x, y in drawbot space
+    height = 2000,
+    width = 1500,
+    # origin = (1200,1000),  # relative to ?
+    outline = { 'visible': False, 'swidth': 4, 'color': [1,0,0,1] },
+    clip = False,
+    sequences = [
+        {
+            'margintop': 50,
+            'lines': [
+                dict(spacing=0,   width=1, color=ruleup.colblack, type='dashed', connectwt=0, changeindent=True),
+                dict(spacing=63,   width=1, color=ruleup.colblack, type='dashed', connectwt=0, changeindent=False),
+                dict(spacing=50,   width=2, color=ruleup.colgreen, type='solid', connectwt=0, changeindent=False),
+                dict(spacing=300,  width=2, color=ruleup.colgreen, type='dashed',  connectwt=2, changeindent=False),
+                dict(spacing=125,  width=2, color=ruleup.colgreen, type='solid',  connectwt=2, changeindent=False),
+                dict(spacing=50,   width=1, color=ruleup.colblack, type='dashed', connectwt=0, changeindent=False),
+                dict(spacing=50,  width=2, color=ruleup.colblue, type='solid',  connectwt=0, changeindent=True),
+                dict(spacing=53,   width=1, color=ruleup.colblue, type='dashed', connectwt=0, changeindent=False),
+                dict(spacing=22,   width=2, color=ruleup.colblue, type='solid', connectwt=0, changeindent=False),
+                ],
+            'indents': [
+                (175, 550), (0, 1300),
+                ],
+            'marginbottom': 100  # space between header and sequences
+        },
+        {
+            'margintop': 50,
+            'lines': [
+                dict(spacing=0,   width=1, color=ruleup.colblack, type='dashed', connectwt=0, changeindent=True),
+                dict(spacing=63,   width=1, color=ruleup.colblack, type='dashed', connectwt=0, changeindent=False),
+                dict(spacing=50,   width=2, color=ruleup.colgreen, type='solid', connectwt=0, changeindent=False),
+                dict(spacing=300,  width=2, color=ruleup.colgreen, type='dashed',  connectwt=2, changeindent=False),
+                dict(spacing=125,  width=2, color=ruleup.colgreen, type='solid',  connectwt=2, changeindent=False),
+                dict(spacing=50,   width=1, color=ruleup.colblack, type='dashed', connectwt=0, changeindent=False),
+                dict(spacing=50,  width=2, color=ruleup.colblue, type='solid',  connectwt=0, changeindent=True),
+                dict(spacing=53,   width=1, color=ruleup.colblue, type='dashed', connectwt=0, changeindent=False),
+                dict(spacing=22,   width=2, color=ruleup.colblue, type='solid', connectwt=0, changeindent=False),
+                ],
+            'indents': [
+                (175, 550), (0, 1300),
+                ],
+            'marginbottom': 100  # space between header and sequences
+        }
+    ]
 )
 
 # Draw page
 newDrawing()
 
 page.setup()
-page.draw_boxes()
+page.draw_frames()
 
-print(page.frame)
+tile_main.draw()
+
 print(page.frame['top'])
 
 endDrawing()                      # advised by drawbot docs
--- ruleup.py
+++ ruleup.py
@@ -10,31 +10,29 @@ layouts = {
     'A4L': { 'page': { 'height' : 2100, 'width': 2970 }, 'frame': { 'top' : 200, 'right': 200, 'bottom': 200, 'left': 200 } },   # A4 portrait, 20mm frame margins
 }
 
-class Box:
-    def __init__(self, boxx, boxy, boxw, boxh, revspecs):
-        self.boxx = boxx
-        self.boxy = boxy
-        self.boxw = boxw
-        self.boxh = boxh
-        specs = {
-            'top' : 0, 'right': 0, 'bottom': 0, 'left': 0, 'width': 0, 'color': [0,0,0,1], 'fill': [1,1,1,1], 'visible': True }
-        specs.update(revspecs)
-        for key, value in specs.items():
-            self.__setattr__(key, value)
+colblack = [0,0,0,1]
+colgreen = [0,1,0,1]
+colblue = [0,0,1,1]
+colred = [1,0,0,1]
 
-    def draw_box(self):
-        with savedState():
-            # Draw full box
-            strokeWidth(0)
-            stroke(0, 0, 0, 1)
-            fill(self.fill[0], self.fill[1], self.fill[2], self.fill[3])
-            rect(self.boxx, self.boxy, self.boxw, self.boxh)
-            # Cut out center
-            strokeWidth(self.width)
-            stroke(self.color[0], self.color[1], self.color[2], self.color[3]) 
-            fill(1, 1, 1, 1)
-            rect(self.boxx + self.left, self.boxy + self.bottom, self.boxw - self.left - self.right, self.boxh - self.bottom - self.top)
+def draw_box(x, y, w, h, s, c): # stroke width, color
+    with savedState():
+        strokeWidth(s)
+        stroke(c[0], c[1], c[2], c[3]) 
+        fill(None)
+        rect(x, y, w, h)
 
+def draw_line(x, y, w, h, s, c, t):   # start x, start y, length, stroke width, color, type
+    with savedState():
+        fill(None)
+        stroke(c[0], c[1], c[2], c[3]) 
+        strokeWidth(s)
+        lineCap("round")
+        if t == 'dashed':
+            lineDash(10, 6)
+        else:
+            lineDash(None)
+        line((x, y), (x + w, y + h))
 
 class Canvas:
     def __init__(self, **kwargs):
@@ -45,16 +43,135 @@ class Canvas:
         settings.update(kwargs)
         for key, value in settings.items():
             self.__setattr__(key, value)
+        dleftx = self.frame['left'] + self.mat['left'] + self.design['left']
+        drightx = self.pagewidth - self.frame['right'] - self.mat['right'] - self.design['right']
+        dtopy = self.pageheight - self.frame['top'] - self.mat['top'] - self.design['top']
+        dbottomy = self.frame['bottom'] + self.mat['bottom'] + self.design['bottom']
+        dmidx = dleftx + (drightx - dleftx) / 2
+        dmidy = dbottomy + (dtopy - dbottomy) / 2
+        self.dwidth = drightx - dleftx
+        self.dheight = dtopy - dbottomy
+        self.dtopl = (dleftx, dtopy)
+        self.dtopc = (dmidx, dtopy)
+        self.dtopr = (drightx, dtopy)
+        self.dmidl = (dleftx, dmidy)
+        self.dmidc = (dmidx, dmidy)
+        self.dmidr = (drightx, dmidy)
+        self.dbottoml = (dleftx, dbottomy)
+        self.dbottomc = (dmidx, dbottomy)
+        self.dbottomr = (drightx, dbottomy)
 
     def setup(self):
         size(self.pagewidth, self.pageheight)
         
-    def draw_boxes(self):
+    def draw_frames(self):
+        framewd = self.pagewidth - self.frame['left'] - self.frame['right']
+        frameht = self.pageheight - self.frame['bottom'] - self.frame['top']
+        matwd = self.pagewidth - self.frame['left'] - self.frame['right'] - self.mat['left'] - self.mat['right']
+        math = self.pageheight - self.frame['bottom'] - self.frame['top'] - self.mat['bottom'] - self.mat['top']
         if self.frame['visible']:
-            frame = Box( revspecs = self.frame, boxx = 0, boxy = 0, boxw = self.pagewidth, boxh = self.pageheight )
-            frame.draw_box()
+            frame = Frame( revspecs = self.frame, framex = 0, framey = 0, framew = self.pagewidth, frameh = self.pageheight )
+            frame.draw_box_orig()
         if self.mat['visible']:
-            mat = Box( revspecs = self.mat, boxx = self.frame['left'], boxy = self.frame['bottom'], boxw = self.pagewidth - self.frame['left'] - self.frame['right'], boxh = self.pageheight - self.frame['bottom'] - self.frame['top'] )
-            mat.draw_box()
+            mat = Frame( revspecs = self.mat, framex = self.frame['left'], framey = self.frame['bottom'], framew = framewd, frameh = frameht )
+            mat.draw_box_orig()
+        if self.design['visible']:
+            design = Frame( revspecs = self.design, framex = self.frame['left'] + self.mat['left'], framey = self.frame['bottom'] + self.mat['bottom'], framew = matwd, frameh = math )
+            design.draw_box_orig()
+
+class Frame:
+    def __init__(self, framex, framey, framew, frameh, revspecs):
+        self.framex = framex
+        self.framey = framey
+        self.framew = framew
+        self.frameh = frameh
+        specs = {
+            'top' : 0, 'right': 0, 'bottom': 0, 'left': 0, 'swidth': 0, 'color': [0,0,0,1], 'fill': [1,1,1,1], 'visible': True }
+        specs.update(revspecs)
+        for key, value in specs.items():
+            self.__setattr__(key, value)
+
+    def draw_box_orig(self):
+        with savedState():
+            # Draw full box
+            strokeWidth(0)
+            stroke(0, 0, 0, 1)
+            fill(self.fill[0], self.fill[1], self.fill[2], self.fill[3])
+            rect(self.framex, self.framey, self.framew, self.frameh)
+            # Cut out center
+            strokeWidth(self.swidth)
+            stroke(self.color[0], self.color[1], self.color[2], self.color[3]) 
+            fill(1, 1, 1, 1)
+            rect(self.framex + self.left, self.framey + self.bottom, self.framew - self.left - self.right, self.frameh - self.bottom - self.top)
+
+class Outline:
+    def __init__(self, outlinex, outliney, outlinew, outlineh, revspecs):
+        self.outlinex = outlinex
+        self.outliney = outliney
+        self.outlinew = outlinew
+        self.outlineh = outlineh
+        specs = { 'swidth': 0, 'color': [0,0,0,1], 'visible': True }
+        specs.update(revspecs)
+        for key, value in specs.items():
+            self.__setattr__(key, value)
+
+    def draw_outline(self):
+        if self.visible:
+            with savedState():
+                strokeWidth(self.swidth)
+                stroke(self.color[0], self.color[1], self.color[2], self.color[3]) 
+                fill(None)
+                rect(self.outlinex, self.outliney, self.outlinew, self.outlineh)
+
+class Tile:
+    def __init__(self, **kwargs):
+        settings = {
+            #'height': 2970,
+            #'width': 2100,
+        }
+        settings.update(kwargs)
+        for key, value in settings.items():
+            self.__setattr__(key, value)
+        leftx = self.align[0] + self.offset[0]
+        midx = leftx + (self.width/2)
+        rightx = leftx + self.width
+        topy = self.align[1] + self.offset[1]
+        midy = topy - (self.height/2)
+        bottomy = topy - self.height
+        self.topl = (leftx, topy)
+        self.topc = (midx, topy)
+        self.topr = (rightx, topy)
+        self.midl = (leftx, midy)
+        self.midc = (midx, midy)
+        self.midr = (rightx, midy)
+        self.bottoml = (leftx, bottomy)
+        self.bottomc = (midx, bottomy)
+        self.bottomr = (rightx, bottomy) 
+
+    def draw(self):
+        if self.visible:
+            if self.outline['visible']:
+                draw_box(self.bottoml[0], self.bottoml[1], self.width, self.height, self.outline['swidth'], self.outline['color'])
+            hpos = self.topl[0]
+            vpos = self.topl[1]
+            length = self.width
+            indent = 0
+            for s in self.sequences:
+                indentcount = 0
+                vpos = vpos - s['margintop']
+                for lin in s['lines']:
+                    if lin["changeindent"]:
+                        indent = s['indents'][indentcount][0]
+                        length = s['indents'][indentcount][1]
+                        if indent + length > self.width:
+                            print("Line", indentcount, "extends beyond design width:", indent + length)
+                        indentcount = indentcount + 1
+                    vpos = vpos - lin['spacing']
+                    draw_line(hpos + indent, vpos, length, 0, lin['width'], lin['color'], lin['type'])
+                    if lin["connectwt"] >0:
+                        draw_line(hpos + indent, vpos, 0, lin['spacing'], lin['width'], lin['color'], 'solid')
+                        draw_line(hpos + indent + length, vpos, 0, lin['spacing'], lin['width'], lin['color'], 'solid')
+                vpos = vpos - s['marginbottom']
+    
 
 
